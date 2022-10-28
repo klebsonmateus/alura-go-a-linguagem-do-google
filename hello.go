ff8ba6875223502c3e1c3b89b3eff064aea29266
Klebson Mateus <klebsonbm@gmail.com>
2022-10-28 13:53:52 -0300

    Finalizando capítulo 02: Exibindo o menu

--- hello.go
+++ hello.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá mundo")
+	nome := "Klebson"
+	versao := 1.1
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("O comando escolhido foi", comando)
 }
